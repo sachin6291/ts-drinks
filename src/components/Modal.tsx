0705532ee856198b4a7eec191f554053073dc3c2
Sachin Tekchandani <sachin6291@gmail.com>
2024-04-30 08:48:22 +0100

    created Favorite

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -74,6 +74,19 @@ export default function Modal() {
 
                     <p className="text-lg">{selectedRecipe.strInstructions}</p>
 
+                    <div className='mt-5 flex justify-between gap-4'>
+                      <button
+                        type='button'
+                        className='w-full rounded bg-gray-600 p-3 font-semibold uppercase text-white shadow hover:bg-gray-500'
+                        onClick={closeModal}
+                      > Close</button>
+
+                      <button
+                        type='button'
+                        className='w-full rounded bg-orange-600 p-3 font-semibold uppercase text-white shadow hover:bg-orange-500'
+                      > add to favorite</button>
+                    </div>
+
                 </Dialog.Panel>
               </Transition.Child>
             </div>
--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,7 +1,9 @@
 import { create } from "zustand"
 import { devtools } from "zustand/middleware"
 import { RecipeSliceType, createRecipeSlice } from "./recipeSlice"
+import { FavoritesSliceType, createFavoritesSlice } from "./favoriteSlice"
 
-export const useAppStore = create<RecipeSliceType>()(devtools((...a)=>({
-    ...createRecipeSlice(...a)
+export const useAppStore = create<RecipeSliceType & FavoritesSliceType>()(devtools((...a)=>({
+    ...createRecipeSlice(...a),
+    ...createFavoritesSlice(...a)
 })))
