aa477f15964571d55e52162b485e45a8ebaf9490
Sachin Tekchandani <sachin6291@gmail.com>
2024-05-04 08:13:19 +0100

    optimize react router dom with lazy and Suspense

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,10 @@
+import { lazy, Suspense } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import IndexPage from "./pages/IndexPage"
-import Favorite from "./pages/Favorite"
 import Layout from "./layout/Layout"
+
+
+const Favorite = lazy(()=> import('./pages/Favorite'))
 
 export default function AppRouter() {
   return (
@@ -9,7 +12,10 @@ export default function AppRouter() {
         <Routes>
           <Route element={<Layout/>}>
             <Route path="/" element={<IndexPage/>} index/>
-            <Route path="/favorite" element={<Favorite/>}/>
+            <Route path="/favorite" element={
+              <Suspense fallback="Loading...">
+                <Favorite/>
+              </Suspense> }/>
           </Route>
         </Routes>
     </BrowserRouter>
